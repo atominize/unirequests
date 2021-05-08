d388b7687f64090f7e3559d8d969efdd8e228210
atominize <atominize@gmail.com>
2021-05-08 20:58:32 +0000

    working on appbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import { Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
+import useStyles from "./styles";
+
+import Home from "../src/components/home/Home";
+import Login from "../src/components/login/Login";
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="sticky">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              UniRequests
+            </Typography>
+            <Button color="inherit">
+              <Link to={"/"} className={classes.link}>
+                Home
+              </Link>{" "}
+            </Button>
+            <Button color="inherit">
+              <Link to={"/login"} className={classes.link}>
+                Login
+              </Link>{" "}
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Switch>
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/" render={() => <Home />} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
