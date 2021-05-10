2e4d1911735fea4d907c7b4ce4f824bd7b891ec2
atominize <atominize@gmail.com>
2021-05-10 13:50:11 +0000

    worked on home page, more to be done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,20 @@
+import { Container, Typography } from "@material-ui/core";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Container maxWidth="md" align="center">
+        <Typography variant="h4" color="primary">
+          Welcome To UniRequests
+        </Typography>
+        <Typography variant="h6" color="textSecondary">
+          We Help You To Obtain Your Documents From KNUST. Documents Such As
+          Transcripts, Certified Copies Of Certificates And Transcripts,
+          Recommendation Letters And English Proficiency.
+        </Typography>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
